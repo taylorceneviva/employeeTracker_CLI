be36d6b55c3425b1079accd891b7fbacbc1563f8
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-01-19 13:19:56 -0500

    fixed broken table

--- schema.sql
+++ schema.sql
@@ -1,23 +1,12 @@
 -- Drop the database if it exists
 DROP DATABASE IF EXISTS employeeTracker_db;
 
--- Create the database
+-- Create the database/ Use database
 CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
 -- Create tables
-CREATE TABLE employee (
-    
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR (30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES manager(id)
-
-);
 
 CREATE TABLE department (
     
@@ -35,4 +24,15 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+CREATE TABLE employee (
+    
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES employee(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
 
