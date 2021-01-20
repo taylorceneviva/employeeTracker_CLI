a27c93b5aa629118ccbacff6c63d6138d59cf204
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-01-19 20:45:01 -0500

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-//const cTable = require('console.table');
+//const consoleTable = require('console.table');
 
 
 var connection = mysql.createConnection({
@@ -11,21 +11,79 @@ var connection = mysql.createConnection({
   database: "employeeTracker_db"
 });
 
-connection.connect(function(err) {
-    if (err) throw err
-    console.log("Connected as Id" + connection.threadId)
-    
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log("connected!");
+    createPrompt();
 });
 
 function createPrompt() {
-    inquirer.prompt([
-    {
-        type: "list",
-        message: "What would you like to do?",
-        name: "choice",
-        choices: ["View All Employees", "View Employees By Role","View Empolyees By Deparment","Add Employee","Add Role","Add Department", "Update Employee Role"]
-    },
-])
+        inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: ["View All Employees", "View Employees By Role","View Empolyees By Deparment","Add Employee","Add Role","Add Department", "Update Employee Role"]
+        }
+    ]).then(function(response) {
+    switch(response.choice) {
+        case "View All Employees":
+            allEmployees();
+            break;
+        case "View Employees By Role":
+            byRole();
+            break;
+        case "View Empolyees By Deparment":
+            byDepartment();
+            break;  
+        case "Add Employee":
+            addEmployee();
+            break;       
+        case "Add Role":
+            addRole();
+            break;        
+        case "Add Department":
+            addDepartment();
+            break;  
+        case "Update Employee Role":
+            updateRole();
+            break;
+        default:
+            break;
+    }
+    })   
+
+};
+
+
+function allEmployees(){
+
+
 }
 
-createPrompt();
+function byRole() {
+
+}
+
+function byDepartment() {
+
+}
+
+function addEmployee() {
+
+}
+
+function addRole() {
+
+}
+
+function addDepartment() {
+
+}
+
+
+function updateRole() {
+
+}
+
